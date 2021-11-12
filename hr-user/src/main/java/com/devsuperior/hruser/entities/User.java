0901e5c873590ca0570a78b72798c3c3e21bb545
Rafael <rafa.jo.souza@hotmail.com>
2021-11-12 12:40:16 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -24,6 +25,8 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
